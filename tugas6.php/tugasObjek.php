d4c55a19058e483d3c3fccce381b160963e6c3a5
Putri Meylisa Maulidya <126633935+Putrimeylsa@users.noreply.github.com>
2023-04-02 08:37:58 +0700

    Update tugasObjek.php

--- tugas6.php/tugasObjek.php
+++ tugas6.php/tugasObjek.php
@@ -3,9 +3,9 @@ require_once 'lingkaran.php';
 require_once 'persegiPanjang.php';
 require_once 'segitiga.php';
 
-$lingkaran = new lingkaran(10);
-$persegiPanjang = new persegiPanjang(17, 12);
-$segitiga = new segitiga(13, 9);
+$lingkaran = new lingkaran(25);
+$persegiPanjang = new persegiPanjang(19, 15);
+$segitiga = new segitiga(18, 9);
 
 $ar_data = [$lingkaran, $persegiPanjang, $segitiga];
 
